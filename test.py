6978ea1a4d80e2a26523341eba91c98659bd0982
nosmeri <62924858+nosmeri@users.noreply.github.com>
2021-04-03 21:16:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import discord
+import os
 
 client = discord.Client()
 
@@ -49,4 +50,5 @@ async def on_message(message):
         await message.delete()
         await message.channel.send("욕하지 마세요")
         
-client.run('ODIwMTczNTY3NDUwMTUyOTkw.YExUKA._-PnMuAs8hrlPVLBKHiPoDxlvZk')
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
